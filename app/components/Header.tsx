356ca1d0b9fd480e4439b97ede43ea1f4c6eba16
irxd <irsyart@gmail.com>
2025-07-30 06:45:03 +0700

    chore: remove unused code
    (cherry picked from commit 8b53094006a28c7bb9e36f1e1420c75ac1c7415b)

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
       <div className="h-16"></div>
       <div className="py-6 sticky top-0 z-10 mix-blend-difference px-4 sm:px-0">
         <div className="container mx-auto flex items-center text-sm md:text-md tracking-tight text-white">
-          <h1 className="font-normal">Muhammad Irsyad</h1>
+          <h1>Muhammad Irsyad</h1>
           <div className="flex-grow mx-6 h-[1px] bg-foreground"></div>
           <nav className="space-x-6">
             <a href="#works">
