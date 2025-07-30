d2a4dc6244251ee40f1267035da1d545c68e0ed2
irxd <irsyart@gmail.com>
2025-07-30 07:17:31 +0700

    feat: update links
    (cherry picked from commit b7ee70aa6879b81a5f7bcbf66ab9ba50cd6a40c6)

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -9,11 +9,11 @@ export default function Footer() {
 
   return (
     <div
-      className='relative h-[240px]'
+      className='relative h-[250px] md:h-[256px]'
       style={{ clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)" }}
     >
-      <footer className='fixed bottom-0 h-[240px] w-full'>
-        <div className="py-20 md:py16 bg-foreground text-black tracking-tight">
+      <footer className="fixed bottom-0 h-[250px] md:h-[256px] w-full">
+        <div className="py-22 md:py16 bg-foreground text-black tracking-tight">
           <div className="container mx-auto flex items-center px-4 sm:px-0">
             <h1 className={
               twMerge(
@@ -27,13 +27,13 @@ export default function Footer() {
           </div>
           <div className="container mx-auto flex items-center mt-6 text-sm md:text-md text-black px-4 sm:px-0">
             <div className='flex flex-row space-x-6'>
-              <a href="#mail">
+              <a href="mailto:irsyart@gmail.com">
                 Mail
               </a>
-              <a href="#github">
+              <a href="https://github.com/irxd">
                 Github
               </a>
-              <a href="#linkedin">
+              <a href="https://linkedin.com/in/irxd">
                 LinkedIn
               </a>
             </div>
@@ -41,7 +41,7 @@ export default function Footer() {
             <p className="">©{new Date().getFullYear()}</p>
           </div>
         </div>
-      </footer>
-    </div>
+      </footer >
+    </div >
   );
 }
--- app/components/Works.tsx
+++ app/components/Works.tsx
@@ -11,7 +11,7 @@ const redaction35Italic = localFont({
 export default function Works() {
 
   return (
-    <div className="min-h-screen bg-background text-white relative">
+    <div className="min-h-screen bg-background text-white relative" id="works">
       <div className="container mx-auto pb-24 md:pb-64 pt-16 md:pt-32 px-4 sm:px-0">
         <div className="flex items-center">
           <h1 className={
@@ -47,7 +47,7 @@ export default function Works() {
       <div className="h-[2vw] w-[2vw] bg-foreground absolute bottom-[2vw]" />
       <div className="h-[2vw] w-[2vw] bg-foreground absolute bottom-0 right-0" />
       <div className="h-[2vw] w-[2vw] bg-foreground absolute bottom-0 right-[2vw]" />
-      <div className="h-[2vw] w-[2vw] bg-foreground absolute bottom-[2vw] right-0" />
+      <div className="h-[2vw] w-[2vw] bg-foreground absolute bottom-[2vw] right-0" id="contact" />
     </div>
   );
 }
