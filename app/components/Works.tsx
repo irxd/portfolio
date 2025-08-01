5d10dd33e16a156720a5270018361e97985631a8
Irsyad <irsyart@gmail.com>
2025-08-01 17:17:15 +0700

    feat: add more work images

--- app/components/Works.tsx
+++ app/components/Works.tsx
@@ -1,8 +1,5 @@
-"use client";
-
 import localFont from "next/font/local";
 import Image from "next/image";
-import { useState } from "react";
 import { twMerge } from "tailwind-merge";
 
 const redaction35Italic = localFont({
@@ -10,12 +7,10 @@ const redaction35Italic = localFont({
 });
 
 export default function Works() {
-  const [hovered, setHovered] = useState<number | null>(null);
-  // const [selected, setSelected] = useState<number | null>(null);
 
   return (
     <div className="min-h-screen bg-black text-white relative">
-      <div className="container mx-auto">
+      <div className="container mx-auto pb-64">
         <div className="flex items-center">
           <h1 className={
             twMerge(
@@ -31,14 +26,9 @@ export default function Works() {
           <div>
             <div
               className="flex items-center mt-24 text-xl"
-              onMouseEnter={() => setHovered(1)}
-              onMouseLeave={() => setHovered(null)}
-            // onClick={() => setSelected(1)}
             >
               <div>First Media</div>
-              <div className={twMerge(
-                "flex-grow mx-6 h-[1px]",
-                hovered === 1 ? "bg-white" : "bg-black")} />
+              <div className="flex-grow mx-6 h-[1px] bg-white" />
               <div>2025</div>
             </div>
             <div className="space-y-4">
@@ -84,17 +74,55 @@ export default function Works() {
             </div>
           </div>
 
-          <div
-            className="flex items-center mt-16 text-xl"
-            onMouseEnter={() => setHovered(2)}
-            onMouseLeave={() => setHovered(null)}
-          // onClick={() => setSelected(2)}
-          >
-            <div>Parcel Daily</div>
-            <div className={twMerge(
-              "flex-grow mx-6 h-[1px]",
-              hovered === 2 ? "bg-white" : "bg-black")} />
-            <div>2024</div>
+          <div>
+            <div
+              className="flex items-center mt-24 text-xl"
+            >
+              <div>Parcel Daily</div>
+              <div className="flex-grow mx-6 h-[1px] bg-white" />
+              <div>2024</div>
+            </div>
+            <div className="space-y-4">
+              <div className="text-sm text-[#cacaca]">
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+              </div>
+              <div className="space-y-4 mt-8 mb-8">
+                <div className="max-w-1/2">
+                  It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution.
+                </div>
+                <div>Fullstack</div>
+                <div>React ▪ React Native ▪ Vue.js ▪ Typescript ▪ Jest ▪ React Native Testing Library</div>
+              </div>
+              <div className="flex flex-row space-x-2">
+                <div className="w-1/3">
+                  <Image
+                    width={600}
+                    height={300}
+                    src="/images/pd-1.png"
+                    alt="First Media Project"
+                    className="w-full h-auto"
+                  />
+                </div>
+                <div className="w-1/3">
+                  <Image
+                    width={600}
+                    height={400}
+                    src="/images/pd-2.png"
+                    alt="First Media Project 2"
+                    className="w-full h-auto"
+                  />
+                </div>
+                <div className="w-1/3">
+                  <Image
+                    width={600}
+                    height={400}
+                    src="/images/pd-3.png"
+                    alt="First Media Project 3"
+                    className="w-full h-auto"
+                  />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
