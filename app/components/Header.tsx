7347fb5d494ed042d02cdeef316c8eff34e6d50c
irxd <irsyart@gmail.com>
2025-08-02 19:10:17 +0700

    feat: update header link

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,19 +1,20 @@
-"use client";
+'use client'
 
-import gsap from 'gsap';
-import { useEffect, useRef } from 'react';
+import gsap from 'gsap'
+import Link from 'next/link'
+import { useEffect, useRef } from 'react'
 
 export default function Header() {
-  const lineRef = useRef<HTMLDivElement>(null);
+  const lineRef = useRef<HTMLDivElement>(null)
 
   const handleScroll = (e: React.MouseEvent<HTMLAnchorElement>, id: string) => {
-    e.preventDefault();
-    const element = document.getElementById(id);
+    e.preventDefault()
+    const element = document.getElementById(id)
     if (element) {
       // @ts-expect-error Lenis already make this global
-      window.lenis?.scrollTo(element, { offset: 0 });
+      window.lenis?.scrollTo(element, { offset: 0 })
     }
-  };
+  }
 
   useEffect(() => {
     if (lineRef.current) {
@@ -26,27 +27,28 @@ export default function Header() {
           scaleX: 1,
           duration: 1.5,
           ease: 'power3.inOut',
-        }
-      );
+        },
+      )
     }
-  }, []);
+  }, [])
 
   return (
     <>
-      <header className="py-6 fixed top-0 z-10 mix-blend-difference px-4 sm:px-0 w-full">
+      <header className="py-6 fixed top-0 z-20 px-4 sm:px-0 w-full bg-black">
         <div className="container mx-auto flex items-center text-sm md:text-lg tracking-tight text-white">
-          <p>Muhammad Irsyad</p>
-          <div ref={lineRef} className="flex-grow mx-6 h-[1px] bg-foreground origin-left"></div>
+          <Link href="/">Muhammad Irsyad</Link>
+          <div
+            ref={lineRef}
+            className="flex-grow mx-6 h-[1px] bg-foreground origin-left"
+          ></div>
           <nav className="space-x-6">
-            <a className="cursor-pointer" onClick={(e) => handleScroll(e, 'works')}>
-              Works
-            </a>
-            <a className="cursor-pointer" onClick={(e) => handleScroll(e, 'contact')}>
+            <Link href="/works">Works</Link>
+            <Link href="none" onClick={(e) => handleScroll(e, 'contact')}>
               Contact
-            </a>
+            </Link>
           </nav>
         </div>
       </header>
     </>
-  );
+  )
 }
