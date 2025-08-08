4f566661b1950d656864c0c48e1a1583f72c2d01
irxd <irsyart@gmail.com>
2025-08-08 10:54:18 +0700

    chore: remove unused font

--- app/components/SelectedWorks.tsx
+++ app/components/SelectedWorks.tsx
@@ -15,10 +15,6 @@ const redaction35Italic = localFont({
   src: '../fonts/Redaction_35-Italic.woff2',
 })
 
-const redaction20 = localFont({
-  src: '../fonts/Redaction_20-Regular.woff2',
-})
-
 export default function SelectedWorks() {
   const lineRef = useRef<HTMLDivElement>(null)
 
